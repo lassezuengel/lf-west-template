ef428b518bf7071a93bb66979f97d817f750e70e
lassezuengel <lassezuengel@gmail.com>
2025-12-28 23:45:04 +0100

    Add simpler, less convoluted server-client

---
+++ apps/simple_client/src/simple-client.c
@@ -0,0 +1,261 @@
+/* echo-client.c - Simplified IPv6 TCP echo client */
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(net_echo_client_sample, LOG_LEVEL_DBG);
+
+#include <errno.h>
+#include <stdio.h>
+#include <zephyr/kernel.h>
+
+#include <zephyr/posix/sys/eventfd.h>
+
+#include <zephyr/net/conn_mgr_monitor.h>
+#include <zephyr/net/net_event.h>
+#include <zephyr/net/net_mgmt.h>
+#include <zephyr/net/socket.h>
+
+#include "common.h"
+
+#define APP_BANNER "Run echo client"
+
+#define INVALID_SOCK (-1)
+
+#define EVENT_MASK (NET_EVENT_L4_CONNECTED | NET_EVENT_L4_DISCONNECTED)
+
+const char lorem_ipsum[] = "LOREM_IPSUM";
+const int ipsum_len = sizeof(lorem_ipsum) - 1;
+
+APP_DMEM struct sample_data conf = {
+  .proto = "IPv6",
+  .tcp_sock = INVALID_SOCK,
+  .counter = 0,
+};
+
+static APP_BMEM struct pollfd fds[2];
+static APP_BMEM int nfds;
+
+static APP_BMEM bool connected;
+
+K_SEM_DEFINE(run_app, 0, 1);
+
+static struct net_mgmt_event_callback mgmt_cb;
+
+// Prepares the fds for polling.
+static void prepare_fds(void) {
+  nfds = 0;
+
+  fds[nfds].fd = eventfd(0, 0);
+  fds[nfds].events = POLLIN;
+  nfds++;
+
+  if (conf.tcp_sock >= 0) {
+    fds[nfds].fd = conf.tcp_sock;
+    fds[nfds].events = POLLIN;
+    nfds++;
+  }
+}
+
+// Waits for events on the prepared fds.
+static void wait(void) {
+  int ret;
+
+  ret = poll(fds, nfds, -1);
+  if (ret < 0) {
+    LOG_ERR("Error in poll:%d", errno);
+    return;
+  }
+
+  if (ret > 0 && fds[0].revents) {
+    eventfd_t value;
+    eventfd_read(fds[0].fd, &value);
+    LOG_DBG("Received restart event.");
+    return;
+  }
+}
+
+static int start_tcp(void);
+static int process_tcp_proto(void);
+static void stop_tcp(void);
+
+static int start_tcp_and_fds(void) {
+  LOG_INF("Starting...");
+
+  int ret = start_tcp();
+  if (ret < 0) {
+    return ret;
+  }
+
+  prepare_fds();
+
+  return 0;
+}
+
+static int run_tcp(void) {
+  int ret = process_tcp_proto();
+  if (ret < 0) {
+    return ret;
+  }
+
+  return 0;
+}
+
+// Sets up network event handler for connection management.
+// On connection, it gives the semaphore to allow the client to run.
+static void event_handler(struct net_mgmt_event_callback *cb,
+                          uint32_t mgmt_event, struct net_if *iface) {
+  if ((mgmt_event & EVENT_MASK) != mgmt_event) {
+    return;
+  }
+
+  if (mgmt_event == NET_EVENT_L4_CONNECTED) {
+    LOG_INF("Network connected");
+    connected = true;
+    k_sem_give(&run_app);
+    return;
+  }
+
+  if (mgmt_event == NET_EVENT_L4_DISCONNECTED) {
+    LOG_INF("Network disconnected");
+    connected = false;
+    k_sem_reset(&run_app);
+    return;
+  }
+}
+
+// Initializes network manager and connection event callback.
+static void init_network_manager(void) {
+  if (IS_ENABLED(CONFIG_NET_CONNECTION_MANAGER)) {
+    net_mgmt_init_event_callback(&mgmt_cb,
+                                 event_handler, EVENT_MASK);
+    net_mgmt_add_event_callback(&mgmt_cb);
+
+    conn_mgr_mon_resend_status();
+  } else {
+    connected = true;
+    k_sem_give(&run_app);
+  }
+}
+
+static void start_client() {
+  int i = 0;
+  int ret;
+
+  while (true) {
+    k_sem_take(&run_app, K_FOREVER);
+
+    ret = start_tcp_and_fds();
+
+    while (connected && (ret == 0)) {
+      ret = run_tcp();
+    }
+
+    stop_tcp();
+  }
+}
+
+int main(void) {
+  LOG_INF(APP_BANNER);
+
+  init_network_manager();
+
+  k_thread_priority_set(k_current_get(), THREAD_PRIORITY);
+
+  start_client();
+  return 0;
+}
+
+/* TCP implementation */
+
+#define RECV_BUF_SIZE 128
+
+static ssize_t sendall(int sock, const void *buf, size_t len) {
+  while (len) {
+    ssize_t out_len = send(sock, buf, len, 0);
+
+    if (out_len < 0) {
+      return out_len;
+    }
+    buf = (const char *)buf + out_len;
+    len -= out_len;
+  }
+
+  return 0;
+}
+
+static int send_tcp_data() {
+  int ret;
+
+  ret = sendall(conf.tcp_sock, lorem_ipsum, 12);
+
+  if (ret < 0) {
+    LOG_ERR("%s TCP: Failed to send data, errno %d", conf.proto,
+            errno);
+  }
+
+  return ret;
+}
+
+// Starts TCP connection to the given address and port.
+static int start_tcp_proto(sa_family_t family,
+                           struct sockaddr *addr, socklen_t addrlen) {
+  int ret;
+
+  conf.tcp_sock = socket(family, SOCK_STREAM, IPPROTO_TCP);
+  if (conf.tcp_sock < 0) {
+    LOG_ERR("Failed to create TCP socket (%s): %d", conf.proto,
+            errno);
+    return -errno;
+  }
+
+  ret = connect(conf.tcp_sock, addr, addrlen);
+  if (ret < 0) {
+    LOG_ERR("Cannot connect to TCP remote (%s): %d", conf.proto,
+            errno);
+    ret = -errno;
+  }
+
+  return ret;
+}
+
+static int process_tcp_proto() {
+  int ret, received;
+  char buf[RECV_BUF_SIZE];
+
+  do {
+    if (++conf.counter % 1000 == 0U) {
+      LOG_INF("%s TCP: Exchanged %u packets", conf.proto,
+              conf.counter);
+    }
+
+    k_msleep(100);
+
+    ret = send_tcp_data();
+    if (ret < 0) {
+      break;
+    }
+  } while (true);
+
+  return ret;
+}
+
+// Starts TCP connection to the server.
+static int start_tcp(void) {
+  int ret = 0;
+  struct sockaddr_in6 addr6;
+
+  addr6.sin6_family = AF_INET6;
+  addr6.sin6_port = htons(PEER_PORT);
+  inet_pton(AF_INET6, CONFIG_NET_CONFIG_PEER_IPV6_ADDR,
+            &addr6.sin6_addr);
+
+  return start_tcp_proto(AF_INET6,
+                         (struct sockaddr *)&addr6,
+                         sizeof(addr6));
+}
+
+static void stop_tcp(void) {
+  LOG_INF("Stopping...");
+  if (conf.tcp_sock >= 0) {
+    (void)close(conf.tcp_sock);
+  }
+}
---
+++ apps/simple_server/src/simple-server.c
@@ -0,0 +1,200 @@
+/* simple-server.c - Simplified IPv6 TCP echo server */
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(net_simple_server_sample, LOG_LEVEL_DBG);
+
+#include <errno.h>
+#include <zephyr/kernel.h>
+#include <zephyr/linker/sections.h>
+#include <zephyr/shell/shell.h>
+
+#include <zephyr/net/net_core.h>
+#include <zephyr/net/socket.h>
+#include <zephyr/net/conn_mgr_monitor.h>
+#include <zephyr/net/net_event.h>
+#include <zephyr/net/net_mgmt.h>
+
+#include "common.h"
+
+static struct k_sem quit_lock;
+static struct net_mgmt_event_callback mgmt_cb;
+static bool connected;
+K_SEM_DEFINE(run_app, 0, 1);
+static bool want_to_quit;
+
+K_THREAD_DEFINE(tcp6_thread_id, STACK_SIZE,
+                start_tcp, NULL, NULL, NULL,
+                THREAD_PRIORITY, 0, -1);
+
+#define EVENT_MASK (NET_EVENT_L4_CONNECTED | NET_EVENT_L4_DISCONNECTED)
+
+// Network management event callback handler.
+static void event_handler(struct net_mgmt_event_callback *cb,
+                          uint32_t mgmt_event, struct net_if *iface) {
+  ARG_UNUSED(iface);
+  ARG_UNUSED(cb);
+
+  if ((mgmt_event & EVENT_MASK) != mgmt_event) {
+    return;
+  }
+
+  if (want_to_quit) {
+    k_sem_give(&run_app);
+    want_to_quit = false;
+  }
+
+  if (mgmt_event == NET_EVENT_L4_CONNECTED) {
+    LOG_INF("Network connected");
+
+    connected = true;
+    k_sem_give(&run_app);
+
+    return;
+  }
+
+  if (mgmt_event == NET_EVENT_L4_DISCONNECTED) {
+    if (connected == false) {
+      LOG_INF("Waiting network to be connected");
+    } else {
+      LOG_INF("Network disconnected");
+      connected = false;
+    }
+
+    k_sem_reset(&run_app);
+
+    return;
+  }
+}
+
+// Initializes the connection manager.
+static void init_connection_manager(void) {
+  k_sem_init(&quit_lock, 0, K_SEM_MAX_LIMIT);
+
+  if (IS_ENABLED(CONFIG_NET_CONNECTION_MANAGER)) {
+    net_mgmt_init_event_callback(&mgmt_cb,
+                                 event_handler, EVENT_MASK);
+    net_mgmt_add_event_callback(&mgmt_cb);
+
+    conn_mgr_mon_resend_status();
+  } else {
+    k_sem_give(&run_app);
+  }
+}
+
+int main(void) {
+  LOG_INF("What's up?");
+
+  init_connection_manager();
+
+  k_sem_take(&run_app, K_FOREVER);
+
+  k_thread_start(tcp6_thread_id);
+
+  k_sem_take(&quit_lock, K_FOREVER);
+
+  if (connected) {
+    stop_tcp();
+  }
+  return 0;
+}
+
+/* TCP implementation */
+
+// Creates and binds TCP listening socket.
+static int start_tcp_proto(struct data *data,
+                           struct sockaddr *bind_addr,
+                           socklen_t bind_addrlen) {
+  int optval;
+  int ret;
+
+  data->tcp_sock = socket(bind_addr->sa_family, SOCK_STREAM,
+                          IPPROTO_TCP);
+  if (data->tcp_sock < 0) {
+    LOG_ERR("Failed to create TCP socket (%s): %d", data->proto,
+            errno);
+    return -errno;
+  }
+
+  if (bind_addr->sa_family == AF_INET6) {
+    optval = IPV6_PREFER_SRC_PUBLIC;
+    (void)setsockopt(data->tcp_sock, IPPROTO_IPV6,
+                     IPV6_ADDR_PREFERENCES,
+                     &optval, sizeof(optval));
+
+    optval = 1;
+    (void)setsockopt(data->tcp_sock, IPPROTO_IPV6, IPV6_V6ONLY,
+                     &optval, sizeof(optval));
+  }
+
+  ret = bind(data->tcp_sock, bind_addr, bind_addrlen);
+  if (ret < 0) {
+    LOG_ERR("Failed to bind TCP socket (%s): %d", data->proto,
+            errno);
+    return -errno;
+  }
+
+  ret = listen(data->tcp_sock, 1);
+  if (ret < 0) {
+    LOG_ERR("Failed to listen on TCP socket (%s): %d",
+            data->proto, errno);
+    ret = -errno;
+  }
+
+  return ret;
+}
+
+struct data conf = {
+  .proto = "IPv6",
+  .tcp_sock = -1,
+};
+
+void start_tcp() {
+  struct sockaddr_in6 addr6;
+
+  (void)memset(&addr6, 0, sizeof(addr6));
+  addr6.sin6_family = AF_INET6;
+  addr6.sin6_port   = htons(4242);
+
+  if(start_tcp_proto(&conf, (struct sockaddr *)&addr6, sizeof(addr6)) < 0) {
+    LOG_ERR("Failed to start TCP server");
+    return;
+  }
+
+  // Accept connections in a LOOP
+  while(true) {
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+
+    LOG_INF("Waiting for connection...");
+    int client_sock = accept(conf.tcp_sock, (struct sockaddr *)&client_addr,
+                            &client_addr_len);
+
+    if (client_sock < 0) {
+      LOG_ERR("Accept error: %d", -errno);
+      continue;  // Keep accepting
+    }
+
+    LOG_INF("Client connected!");
+
+    while(true) {
+      char buffer[128];
+      ssize_t recv_len = recv(client_sock, buffer, sizeof(buffer), 0);
+
+      if (recv_len < 0) {
+        LOG_ERR("Receive error: %d", -errno);
+        break;
+      } else if (recv_len == 0) {
+        LOG_INF("Client disconnected");
+        break;
+      }
+
+      LOG_INF("Received %d bytes: %.*s", recv_len, (int)recv_len, buffer);
+    }
+
+    close(client_sock);
+  }
+}
+
+void stop_tcp() {
+
+}
